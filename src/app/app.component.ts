4cd840510d909e8f4db232d336804e3d50864859
bhagath <bhagath@getcustomexposure.com>
2018-01-15 17:53:09 +0530

    getting the index when using ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [` h3 {
-  color: dodgerblue;
-  }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = '';
+    showSecret = false;
+    log = [];
+    onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(new Date());
+    }
 }
